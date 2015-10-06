b9d5deec790420a2c728f626eadcf23d7b27c9ba
kingstan <skaopare@ucalgary.ca>
2015-10-06 15:42:09 -0600

    I made changes to the script

--- extract.sh
+++ extract.sh
@@ -1,1 +1,6 @@
 #Anahit Petoyan, october  6 , 2015
+# Tiffany Timbers, October 6, 2015
+# This script will extract unique elements from a colum
+# of a csv file
+
+cut -f 2 -d',' data/antibiotics.csv | sort | uniq > antibiotics_unique.csv
